5d20778b0bb493e8a0745ab2a2a411bbadc5eee7
bntong <bntong@NN123-HA-02.HS.sd203.org>
2016-02-10 14:24:39 -0600

    Nike
    Fac id

--- Chapter 8 Practice Folder/ButtonViewer.java
+++ Chapter 8 Practice Folder/ButtonViewer.java
@@ -1,23 +1,55 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class ButtonViewer
 {
-    private static final int FRAME_WIDTH = 100;
-    private static final int FRAME_HEIGHT = 60;
+    private static final int FRAME_WIDTH = 200;
+    private static final int FRAME_HEIGHT = 200;
     
-    public static void main(String[] args)
+    private JFrame frame;
+    private JButton button;
+    private JButton button2;
+    private JPanel panel;
+    private JLabel label;
+
+    public ButtonViewer()
     {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click Me!");
-        frame.add(button);
-        
+        this.frame = new JFrame();
+        this.panel = new JPanel();
+        this.label = new JLabel();
+
+        this.button = new JButton("A");
+        this.panel.add(this.button);
+
+        this.panel.add(this.label);
+
+        this.button2 = new JButton("B");
+        this.panel.add(this.button2);
+
         ClickListener listener = new ClickListener();
         button.addActionListener(listener);
-        
+        button2.addActionListener(listener);
+
+        this.frame.add(panel);
+
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        ButtonViewer view = new ButtonViewer();
+    }
+
+    public class ClickListener implements ActionListener
+    {
+        private String name;
+
+        public void actionPerformed(ActionEvent event)
+        {        
+            label.setText("Button " + event.getActionCommand() + " was clicked");
+        }
     }
 }
